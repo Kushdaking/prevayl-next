6bc11085e1fdb042cb1772cabff2635ae4df41c3
Kenny <Kenny@prevaylos.com>
2026-09-23 10:52:36 -0400

    Replace carrier illustrations with detailed interactive fleet workspaces

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import "./resources.css";
 import "./unique-visuals.css";
 import "./solution-workspaces.css";
 import "./integrations.css";
+import "./fleet-workspace.css";
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
 import { brand } from "@/content/site";
